9144d5f823bb30189bb8ac4319dc08d1aff2fc56
dpinart <dpinart@newuibcn.com>
2019-01-25 14:19:22 +0100

    perf():update lit-element 2.0 and some performance
    BREAKING CHANGE: update lit-element 2.0 and some performance

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,5 @@ export const routingActionNamesFactory = (action: string) => {
     const actionsBuilder = actionNameBuilder(prefix);
     return actionsBuilder(action);
 };
+
 export default routingActionNamesFactory;
